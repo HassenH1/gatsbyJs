9d8981b3e022e619648e1923ed5b906604757deb
hassen <hasansaid51@gmail.com>
2020-01-20 11:15:12 -0800

    bio

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,7 +18,7 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'pic.jpg',
-  paragraphOne: 'Hi, my Name is Hassen S. Hassen and I am a software Engineer with a background in customer service. my background in customer service allows me to communicate effectively with both clients and on a team. As a creative problem solver with a passion for learning and continually seek better ways to provide clients with innovative solutions.',
+  paragraphOne: 'Hi, my Name is Hassen S. Hassen and I am a software Engineer with a background in customer service. my background in customer service allows me to communicate effectively with both clients and on a team. I am a creative problem solver with a passion for learning and I continually seek better ways to provide clients with innovative solutions.',
   resume: 'UpdatedRes.pdf', // if no resume, the button will not show up
 };
 
@@ -28,7 +28,7 @@ export const projectsData = [
     id: uuidv1(),
     img: 'clothing.PNG',
     title: 'Clothin Store',
-    info: '',
+    info: 'A simple E-commerce fashion store website developed using MERN stack so Mongodb, Expressjs, Reactjs, Nodejs, and with firebase for user authenication and storage. It has features like Signup, Login, Add to Cart, browing through clothes etc.',
     info2: '',
     url: '',
     repo: 'https://github.com/HassenH1/capstone', // if no repo, the button will not show up
@@ -37,7 +37,7 @@ export const projectsData = [
     id: uuidv1(),
     img: 'adopt1.PNG',
     title: 'Adopt Me',
-    info: '',
+    info: 'Its a game for adopting a pet. Use the swipe right feature of you like an animal or use the swipe left if you do not. Think of it as a wingman for Adopting a Pet wherever you are. If you want to adopt a pet you can look through your history off all the liked pets and choose your favorite and get the contact information.',
     info2: '',
     url: '',
     repo: 'https://github.com/HassenH1/Adopt-Me', // if no repo, the button will not show up
@@ -46,7 +46,7 @@ export const projectsData = [
     id: uuidv1(),
     img: 'gamereview.png',
     title: 'Gamer Review',
-    info: '',
+    info: 'A video game reviews site where users can register and express their opinions on a wide range of titles.',
     info2: '',
     url: '',
     repo: 'https://github.com/HassenH1/project3-react', // if no repo, the button will not show up
@@ -55,7 +55,7 @@ export const projectsData = [
     id: uuidv1(),
     img: 'githubfinder.PNG',
     title: 'Github Finder',
-    info: '',
+    info: 'Github finder is a simple web app that allows someone to search for a Github user and display there most recent repository.',
     info2: '',
     url: '',
     repo: 'https://github.com/HassenH1/TheGithubMiniProject', // if no repo, the button will not show up
